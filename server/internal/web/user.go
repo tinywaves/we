d45825e5da270171c3ae5f3d8313f779ac826459
Denver Zheng <isdhzh@outlook.com>
2023-08-07 01:25:03 +0800

    chore: router group.

--- server/internal/web/user.go
+++ server/internal/web/user.go
@@ -6,10 +6,11 @@ import "github.com/gin-gonic/gin"
 type UserHandler struct{}
 
 func (u *UserHandler) RegisterRouters(server *gin.Engine) {
-	server.POST("/user/signup", u.Signup)
-	server.POST("/user/login", u.Login)
-	server.POST("/user/edit", u.Edit)
-	server.GET("/user/profile", u.Profile)
+	routerGroup := server.Group("/user")
+	routerGroup.POST("/signup", u.Signup)
+	routerGroup.POST("/login", u.Login)
+	routerGroup.POST("/edit", u.Edit)
+	routerGroup.GET("/profile", u.Profile)
 }
 
 func (u *UserHandler) Signup(ctx *gin.Context) {}
